ad42aa0a2278638622d39476a88a0045a049df92
Joerg <joerg.patzer@me.com>
2014-07-08 10:17:58 +0200

    improved apache.sh script

--- apache.sh
+++ apache.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 
-# Install and set Apache ServerName
+# Install Apache
 sudo apt-get install -y apache2
+sudo a2enmod rewrite
+sudo a2enmod headers
+
+# set Apache Settings
 sudo sed -i "s/#ServerRoot.*/ServerName ubuntu/" /etc/apache2/apache2.conf
+sudo sed -i "s|DocumentRoot.*|DocumentRoot /var/www|" /etc/apache2/sites-available/000-default.conf
+test -d /var/www/html && sudo rm -r /var/www/html
 
 # Create a domainname directory for the example
 sudo mkdir -p /var/www/phpinfo.dev
 echo "<?php phpinfo();" | sudo tee /var/www/phpinfo.dev/index.php
 
 # Configure Apache Hosts
-sudo a2enmod rewrite
 vhost="<VirtualHost *:80>
-ServerName phpinfo.dev
-DocumentRoot /var/www/phpinfo.dev
-<Directory \"/var/www/phpinfo.dev\">
-Order allow,deny
-Allow from all
-Require all granted
-AllowOverride All
-</Directory>
+    ServerName phpinfo.dev
+    DocumentRoot /var/www/phpinfo.dev
+    <Directory \"/var/www/phpinfo.dev\">
+        Order allow,deny
+        Allow from all
+        Require all granted
+        AllowOverride All
+    </Directory>
 </VirtualHost>"
 echo "$vhost" | sudo tee /etc/apache2/sites-available/phpinfo.dev.conf
 sudo a2ensite phpinfo.dev
 
-# Reload apache2 service
-sudo service apache2 reload
+# Restart apache2 service
+sudo service apache2 restart
